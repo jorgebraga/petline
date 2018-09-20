c72f66f894a603b8f81b58d7d649b726f6c5310b
Evaldo Jr <evaldojunior1997@hotmail.com>
2018-09-20 17:37:54 -0300

    ADD imagem de logo

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ include "cabecalho.php";
 <h1>Bem vindo a PetLine, Fulano(a)!</h1>
 <p>A PetLine é uma empresa que realiza intermedio entre passeios de cães!</p>
 
+<section> <!-- Ajustar imagem de logo no fundo -->
+  <div id="img-intro">
+    
+  </div>
+</section> 
+
 <?php
 include "rodape.php";
 ?>
