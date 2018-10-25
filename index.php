376d68a6ac71f02dc4452227a2ddfb42c8ed0d6b
Jorge Braga <jorge_fbl@hotmail.com>
2018-10-25 19:54:37 -0300

    Adicionado front página de cadastro

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include "cabecalho.php";
 ?>
 
 <h1>Bem vindo a PetLine, Fulano(a)!</h1>
-<p>A PetLine é uma empresa que realiza intermedio entre passeios de cães!</p>
+<p>A PetLine é uma empresa que realiza intermedio entre passeadores de cães e seus proprietários!</p>
 
 
 <?php
