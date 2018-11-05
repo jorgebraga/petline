95d42ca19a71d426fb3c4ba0d5c938f15b75af04
Jorge Braga <jorge_fbl@hotmail.com>
2018-11-05 20:57:49 -0200

    sobre ajustes

--- sobre.php
+++ sobre.php
@@ -7,6 +7,9 @@ include "cabecalho.php";
     <div id=”cabecalho”>
       <div id="topo">
         <img src="img/backlogo.jpg" alt="" style="width:100%;">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/petline.css">
+        
       </div>
       
     </div>
@@ -16,17 +19,19 @@ include "cabecalho.php";
 <!-- Seção de Sobre-->
 
 <main class="container">
+  <center>
   
-    <div id="quemSomos"class="col-sm-3 col-md-10">
-    <center>
+ 
+  
       <h3>Sobre a PETLINE</h3>
+   
       <p align="justify">
-        Fundada em 2018, a PETLINE será uma empresa criada por alunos do curso de bacharel em Sistemas de Informação da unidade de Águas Claras - DF.</p>
+        &emsp; &emsp; Fundada em 2018, a PETLINE será uma empresa criada por alunos do curso de bacharel em Sistemas de Informação da unidade de Águas Claras - DF.</p>
       <p align="justify">
-        A empresa tem como objetivo atuar no mercado consumidor prestando serviços de qualidade, focando em passeios de animais domésticos no dia a dia. A mesma apresenta os seus serviços por meio de um aplicativo (inicialmente somente para sistema Android), onde é possível que pessoas, possíveis clientes, possam por meio do aplicativo, encontrar profissionais preparados e capacitados para que possam prestar o serviço de passeio com os respectivos animais com a devida qualidade, segurança e confiabilidade.
+        &emsp; &emsp; A empresa tem como objetivo atuar no mercado consumidor prestando serviços de qualidade, focando em passeios de animais domésticos no dia a dia. A mesma apresenta os seus serviços por meio de um aplicativo (inicialmente somente para sistema Android), onde é possível que pessoas, possíveis clientes, possam por meio do aplicativo, encontrar profissionais preparados e capacitados para que possam prestar o serviço de passeio com os respectivos animais com a devida qualidade, segurança e confiabilidade.
       <p align="justify">
-        A PETLINE tem como público alvo, indiretamente, as pessoas residentes em Águas Claras no Distrito Federal, que possuem animais de estimação e não possuem a disponibilidade de horário ou qualquer motivo que o impossibilite de realizar o passeio com os animais. Sendo assim, o foco direto do público alvo da empresa são os animais domésticos que necessitam de um guia em seus passeios.</p>
-    </div>
+        &emsp; &emsp; A PETLINE tem como público alvo, indiretamente, as pessoas residentes em Águas Claras no Distrito Federal, que possuem animais de estimação e não possuem a disponibilidade de horário ou qualquer motivo que o impossibilite de realizar o passeio com os animais. Sendo assim, o foco direto do público alvo da empresa são os animais domésticos que necessitam de um guia em seus passeios.</p>
+ 
       <br>
       <br>
   </center>
@@ -36,46 +41,39 @@ include "cabecalho.php";
 <hr>
 <!-- Seção de Diferencial-->
 <section class="container">
+   <center>
     <div id="diferencial">
-    <center>
-      <h3>Diferencial PETLINE</h3>
-    
-      <div id="diferencial"class="col-sm-3 col-md-10">
-        <P align="justify">
-          Por ser uma empresa formada dentro do curso de sistemas de informação, sua estrutura organizacional é totalmente composta por alunos, onde cada aluno é responsável por determinado setor da empresa.
-        </P>        
-      </div>
-    </center>
+     
+         <h3>Diferencial PETLINE</h3>
+          
+               <P align="justify">
+          &emsp; &emsp; Por ser uma empresa formada dentro do curso de sistemas de informação, sua estrutura organizacional é totalmente composta por alunos, onde cada aluno é responsável por determinado setor da empresa.
+               </P>
     </div>
+        
+
+    </center>
 </section>
 <!-- FIM Seção de Diferencial-->
 <hr>
 <!-- Seção de M.V.V-->  
 <section id="bsc" class="container">
-  <div class="container">
+
     <div class="row">
       <div class="col-sm-4" id="missao">
-        <center>
           <h3>Missão</h3>
-        </center>
-          <p align="justify">Propiciar, de forma acessível e confiável o encontro dinâmico entre passeadores e donos de pets, proporcionando momentos de descanso para os animais e segurança para os seus donos.</p>        
-      </div>
-
+          <p align="justify"> &emsp; &emsp; Propiciar, de forma acessível e confiável o encontro dinâmico entre passeadores e donos de pets, proporcionando momentos de descanso para os animais e segurança para os seus donos.</p>        
+      </div> 
       <div class="col-sm-4" id="visao">
-        <center>
-          <h3 align="center">Visão</h3>
-        </center>
-          <p align="justify">Ser em alguns anos, referência e destaque no ramo de passeio de animais domésticos. </p>        
+          <h3>Visão</h3>
+          <p align="justify"> &emsp; &emsp; Ser em alguns anos, referência e destaque no ramo de passeio de animais domésticos. </p>        
       </div>
-
-      <div class="col-sm-4" id="">
-        <center>
+      <div class="col-sm-4" id="valores">
           <h3>Valores</h3>
-        </center>
-          <p align="justify">Confiabilidade, ética, acessibilidade aos diversos públicos, cuidado e zelo com os animais e donos.</p>    
+          <p align="justify"> &emsp; &emsp; Confiabilidade, ética, acessibilidade aos diversos públicos, cuidado e zelo com os animais e donos.</p>
       </div>
     </div>
-  </div>
+
 </section>
      <br>
 
@@ -83,18 +81,17 @@ include "cabecalho.php";
   <hr>
   <!--Seção de contato-->
   
-  <section id="contatos" class="container">  
-    <div aling="center">
+  
+  <section id="contatos" class="container"> 
       <div class="col-sm-11" id="contato">
-        <h2>Contato</h2>
-        <div><i class="fa fa-envelope-square"> E-mail: passeios.petline@gmail.com</i></div>
-        <div><i class="fa fa-phone-square"> Telefone:</i></div>
-        <div><i class="btn-facebook"> Facebook</i></div>
-        <div><i class="fa fa-twitter-square"> Twitter</i></div>
-        <div><i class="fa fa-instagram"> Instagram</i></div>
+        <h2>Contatos e mídias sociais</h2>
+        <div><i style= "font-size: 17px" class="fa fa-envelope-square"> E-mail: passeios.petline@gmail.com</i></div>
+        <div><i style= "font-size: 17px" class="fa fa-phone-square"> Telefone: xxxx-xxxx</i></div>
+        <div><i style= "font-size: 17px" class="fa fa-facebook-square"> Facebook: facebook.com/petline</i></div>
+        <div><i style= "font-size: 17px" class="fa fa-twitter-square"> Twitter: @petline</i></div>
+        <div><i style= "font-size: 17px" class="fa fa-instagram"> Instagram: instagram.com/petline</i></div>
       </div>
-    </div>
-  </section>
+   </section>
 </div>
   <!-- FIM Seção de contato-->
 
